ec31a0d74efd9e10a45f3bec0adcc025c2aa655d
Francisco-Galindo <paqui10718@gmail.com>
2023-02-25 15:21:46 -0600

    Creando main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+print("Bienvenido!")
+
+print("""
+      +---+ +---+ +---+
+      |   | |   | |   |
+      | A | | B | | C |
+      |   | |   | |   |
+      +---+ +---+ +---+
+""")
+
+input("Introduzca su dinero: ")
+input("Elija el producto: ")
+
+print("Trabajando...")
+
+print("Aquí esta su producto!")
+
+print("Su cambio es...")
+
+print("Adiós!")
