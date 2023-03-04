075fa665e8933aacfb53bb58ed7af7224b62f31f
AngelReyBris <125548315+AngelReyBris@users.noreply.github.com>
2023-03-04 05:03:57 -0600

    Nuevos cambios en la clase Inventario: version final 2

--- Inventario2.py
+++ Inventario2.py
@@ -1,64 +1,81 @@
-class Productos:
-
-    def diccionario(self):
-        productos = {}
-        print (productos)
-pass
-
 # It creates a class called Codigos.
 class Codigos:
 
     def funcion(self):
-        input ("ingrese el codigo: ")
-        #aqui tengo que poner el append para que ingrese a mi dicionario posiblemente
+        diccionario["codigos"].append(input ("ingrese el codigo: \n"))
    
 # It creates a class called Nombres.
 class Nombres:
 
     def funcion(self):
-        input ("ingrese el nombre del producto: \n")
+        diccionario["nombres"].append(input ("ingrese el nombre del producto: \n"))
 
 # It creates a class called Marcas.
 class Marcas:
 
     def funcion(self):
-        input ("ingrese la marca del producto: \n")
+        diccionario["marcas"].append(input ("ingrese la marca del producto: \n"))
     
 
-# The class Caducidades has a function called funcion that takes no arguments and returns nothing.
+# The class Caducidades has a function called funcion that takes no arguments and returns nothing
 class Caducidades:
 
     def funcion(self):
-        input ("ingrese la fecha de caducidad del producto: \n")
+        diccionario["fechas_caducidad"].append(input ("ingrese la fecha de caducidad del producto: \n"))
       
 
-# It creates a class called Precios.
+# The class Precios has a function called funcion that takes no arguments and returns nothing
 class Precios:
 
     def funcion(self):
-        input ("ingrese el precio del producto por unidad: \n")
+        diccionario["precios"].append(input ("ingrese el precio del producto: \n"))
 
-# A class that asks for a volume.
+# It's a class that has a function that takes user input and appends it to a dictionary
 class Volumenes:
 
     def funcion(self):
-        input ("ingrese el volumen (si aplica, sino ingrese cero): \n")
+        diccionario["volumenes"].append(input ("ingrese el volumen en ml del producto (en caso de que no aplique ingresar 0): \n"))
        
 
-# It asks the user to input a weight.
+# The class Pesos is a class that has a function that asks the user to input the weight of the product
+# in grams
 class Pesos:
 
     def funcion(self):
-        input ("ingrese el peso (si aplica, sino ingrese cero): \n")
+        diccionario["pesos"].append(input ("ingrese el peso en gr del producto (en caso de que no aplique ingresar 0): \n"))
   
 
-# A class that asks the user to input the number of units to enter.
+# It creates a class called Ingresos.
 class Ingresos:
 
     def funcion(self):
-        input ("digite la cantidad de unidades a ingresar: \n")
-      
-# Creating an instance of each class.
+        diccionario["cantidades ingresadas"].append(input ("ingrese las unidades del producto a ingresar: \n"))
+
+# The class Iteraciones has a method funcion that prints the data of the new product (product i+1) in
+# the inventory.
+class Iteraciones:
+
+    def funcion(self):
+
+            print(f"Mostrando los datos del nuevo producto (producto {i+1}) dentro del inventario:\n")
+            for key, value in diccionario.items():
+                print(f'{key}, {value}')
+                        
+
+# It's a dictionary that has a list of keys and values.
+diccionario = {
+            "codigos": [],
+            "nombres": [],
+            "marcas": [],
+            "fechas_caducidad": [],
+            "precios": [],
+            "volumenes": [],
+            "pesos": [],
+            "cantidades ingresadas": []
+        }  
+
+# It's creating an instance of each class.
+tamaño = 2      
 codigo = Codigos()
 nombre = Nombres()
 marca = Marcas()
@@ -67,15 +84,17 @@ precio = Precios()
 volumen = Volumenes()
 peso = Pesos()
 ingreso = Ingresos()
-#productos = Productos() #de alguna manera debo rellenar el diccionario, ya sea con algun metodo
-#clase por clase o todas en una clase, o el diccionario por separado.
+iteracion = Iteraciones()
 
-# Iterating through the list of classes and calling the function funcion() on each class.
-for productos in (codigo, nombre, marca, caducidad, precio, volumen, peso, ingreso):
-    productos.funcion()
+# It's a for loop that iterates through the list of classes and calls the function funcion() of each
+# class.
+for i in range(tamaño):
+    for productos in (codigo, nombre, marca, caducidad, precio, volumen, peso, ingreso, iteracion):
+        productos.funcion()
 
-
-
+print("--------------imprimiendo todo el diccionario-----------------------") 
+#esta parte aun falta por desarrollarse pongo mientras una impresion de todo para acordarme.  
+print(diccionario.items())
 
     
     
