e4e7d7a9940b745e6369301057ad3c0ab48452c6
Francisco-Galindo <paqui10718@gmail.com>
2023-03-05 17:21:52 -0600

    fix (Maquina.py): Corrigiendo estilo de impresión de ticket

--- Maquina.py
+++ Maquina.py
@@ -32,7 +32,7 @@ class Maquina():
     def recibirPago(self):
         debePagar = True
 
-        print(f"Son ${self.__total}")
+        print(f"Son ${self.__total:.2f}")
 
         while debePagar:
 
@@ -45,7 +45,7 @@ class Maquina():
             if self.__currPago >= self.__total:
                 debePagar = False
             else:
-                print(f"Todavía faltan ${self.__total - self.__currPago}...")
+                print(f"Todavía faltan ${self.__total - self.__currPago:.2f}...")
 
 
     def darCambio(self):
@@ -56,11 +56,11 @@ class Maquina():
 
         if cambio > self.__dinero:
             print("¡La máquina no tiene suficiente dinero para dar cambio!")
-            print(f"Te devolvemos tus ${self.__currPago}")
+            print(f"Te devolvemos tus ${self.__currPago:.2f}")
             return False
 
         if cambio > 0:
-            print(f"Su cambio es de ${cambio}")
+            print(f"Su cambio es de ${cambio:.2f}")
 
         self.__dinero -= cambio
         self.__dinero += self.__currPago - cambio
@@ -100,16 +100,16 @@ class Maquina():
             ft.write("----------------Ticket de compra:----------------\n\n")
 
             ft.write(f"Fecha de compra: {fechahora}\n\n")
-            ft.write("CODIGO:\t PRECIO:\n")
+            ft.write("CODIGO:\tPRECIO:\n")
 
             for i in self.__infoTicket:
-                ft.write(f"{i['codigo']}\t {i['precio']}\n")
+                ft.write(f"{i['codigo']}\t${i['precio']:.2f}\n")
 
-            ft.write(f"\nTotal\t${self.__total}\n")
-            ft.write(f"Pago\t${self.__currPago}\n")
-            ft.write(f"Cambio\t${self.__currPago - self.__total}\n")
+            ft.write(f"\nTotal\t${self.__total:.2f}\n")
+            ft.write(f"Pago\t${self.__currPago:.2f}\n")
+            ft.write(f"Cambio\t${self.__currPago - self.__total:.2f}\n")
 
-            ft.write("---------------------------------------------------")
+            ft.write("---------------------------------------------------\n")
 
         self.__infoTicket = []
 
