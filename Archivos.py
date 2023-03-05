df86e5683840780b22919084c9091babd2ba7ea4
lued2897 <lued2897@gmail.com>
2023-03-05 12:41:53 -0600

    Update Archivos.py
    cambio a archivos.py, ahora regresa una lista de diccionarios con el codigo del producto como llave

--- Archivos.py
+++ Archivos.py
@@ -2,7 +2,8 @@ import csv
 from Producto import *
 
 def Carga_csv(nombre_archivo:str)->list:
-    """Recibe el nombre del archivo a cargar como cadena de texto y regresa una lista de productos"""
+    """Recibe el nombre del archivo .csv a cargar como cadena de texto y regresa una lista de diccionarios de productos{"codigo": producto}\n
+El archivo debe tener la siguiente estructura: Tipo,Nombre,Precio,Codigo,Marca,Fecha_Caducidad,Peso/Volumen,Cantidad"""
 
     productos=[]
     with open(nombre_archivo,"r") as f:
@@ -10,10 +11,17 @@ def Carga_csv(nombre_archivo:str)->list:
 
         for linea in archivo:
             if linea["Tipo"]=="Bebida":
-                productos.append(Bebida(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
-                                        linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"]))
+                diccionario={linea["Codigo"]: Bebida(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
+                                                linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"]) }
+                productos.append(diccionario)
             elif linea["Tipo"]=="Botana":
-                productos.append(Botana(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
-                                        linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"]))
+                diccionario={linea["Codigo"]: Botana(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
+                                                linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"])}
+                productos.append(diccionario)
+
 
         return productos
+
+prod=Carga_csv("prueba.csv")
+for i in prod:
+    print(i)
