f87f354d6d61b2e5e6184ae9f26e111883c1393c
lued2897 <lued2897@gmail.com>
2023-03-05 15:15:01 -0600

    Revert "Update Archivos.py"
    This reverts commit df86e5683840780b22919084c9091babd2ba7ea4.

--- Archivos.py
+++ Archivos.py
@@ -2,8 +2,7 @@ import csv
 from Producto import *
 
 def Carga_csv(nombre_archivo:str)->list:
-    """Recibe el nombre del archivo .csv a cargar como cadena de texto y regresa una lista de diccionarios de productos{"codigo": producto}\n
-El archivo debe tener la siguiente estructura: Tipo,Nombre,Precio,Codigo,Marca,Fecha_Caducidad,Peso/Volumen,Cantidad"""
+    """Recibe el nombre del archivo a cargar como cadena de texto y regresa una lista de productos"""
 
     productos=[]
     with open(nombre_archivo,"r") as f:
@@ -11,17 +10,10 @@ El archivo debe tener la siguiente estructura: Tipo,Nombre,Precio,Codigo,Marca,F
 
         for linea in archivo:
             if linea["Tipo"]=="Bebida":
-                diccionario={linea["Codigo"]: Bebida(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
-                                                linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"]) }
-                productos.append(diccionario)
+                productos.append(Bebida(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
+                                        linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"]))
             elif linea["Tipo"]=="Botana":
-                diccionario={linea["Codigo"]: Botana(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
-                                                linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"])}
-                productos.append(diccionario)
-
+                productos.append(Botana(linea["Nombre"],linea["Precio"],linea["Codigo"],linea["Marca"],
+                                        linea["Fecha_Caducidad"],linea["Peso/Volumen"],linea["Cantidad"]))
 
         return productos
-
-prod=Carga_csv("prueba.csv")
-for i in prod:
-    print(i)
